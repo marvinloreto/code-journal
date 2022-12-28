522d50ebd28c68435bddd37ae9d4f14bbaad27ea
Marvin Loreto <marvinjloreto@gmail.com>
2022-12-28 23:39:50 +0000

    Listen for submit event on entry form, #1

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
 var imageEntry = document.querySelector('img');
 var imageURL = document.getElementById('photo-URL');
+var entryForm = document.querySelector('form');
 
 function getImage(event) {
   imageEntry.setAttribute('src', imageURL.value);
 }
 
 imageURL.addEventListener('input', getImage);
+
+function saveEntry(event) {
+  event.preventDefault();
+  var entryData = new FormData(entryForm);
+  var dataObject = Object.fromEntries(entryData);
+  dataObject.entryId = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.push(dataObject.entryId);
+  imageEntry.setAttribute('src', 'images/placeholder-image-square.jpg');
+  entryForm.reset();
+}
+
+entryForm.addEventListener('submit', saveEntry);
