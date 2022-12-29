840605099e4059869aa76ac3628d4ab5d8ddaee8
Marvin Loreto <marvinjloreto@gmail.com>
2022-12-29 23:57:11 +0000

    Defined function renderEntry. #2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var imageEntry = document.querySelector('img');
-var imageURL = document.getElementById('photo-URL');
+var imageURL = document.getElementById('photoURL');
 var entryForm = document.querySelector('form');
 
 function getImage(event) {
@@ -14,9 +14,49 @@ function saveEntry(event) {
   var dataObject = Object.fromEntries(entryData);
   dataObject.entryId = data.nextEntryId;
   data.nextEntryId++;
-  data.entries.push(dataObject.entryId);
+  data.entries.push(dataObject);
   imageEntry.setAttribute('src', 'images/placeholder-image-square.jpg');
   entryForm.reset();
 }
 
 entryForm.addEventListener('submit', saveEntry);
+
+function renderEntry(entry) {
+  var newDivContainer = document.createElement('div');
+  newDivContainer.setAttribute('class', 'container');
+
+  var newContainerRow = document.createElement('div');
+  newContainerRow.setAttribute('class', 'row');
+  newDivContainer.appendChild(newContainerRow);
+
+  var newRowColHalfPic = document.createElement('div');
+  newRowColHalfPic.setAttribute('class', 'column-half');
+  newContainerRow.appendChild(newRowColHalfPic);
+
+  var newColHalfImage = document.createElement('img');
+  newColHalfImage.setAttribute('src', entry.photoURL);
+  newRowColHalfPic.appendChild(newColHalfImage);
+
+  var newRowColHalfText = document.createElement('div');
+  newRowColHalfText.setAttribute('class', 'column-half');
+  newContainerRow.appendChild(newRowColHalfText);
+
+  var entryTitle = document.createElement('h3');
+  entryTitle.textContent = entry.title;
+  newRowColHalfText.appendChild(entryTitle);
+
+  var entryNotes = document.createElement('p');
+  entryNotes.textContent = entry.notes;
+  newRowColHalfText.appendChild(entryNotes);
+
+  var list = document.querySelector('ul');
+
+  for (let i = 0; i < data.entries.length; i++) {
+    var journalEntry = data.entries[i];
+    list.appendChild(journalEntry);
+  }
+
+  return newDivContainer;
+}
+
+document.addEventListener('DOMContentLoaded', renderEntry);
