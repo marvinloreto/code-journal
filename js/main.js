2c555a800bfcb3ae1ae1ce08b867849d42dbddb4
Marvin Loreto <marvinjloreto@gmail.com>
2023-01-03 06:04:22 +0000

    Completed issue-2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 var imageEntry = document.querySelector('img');
 var imageURL = document.getElementById('photoURL');
-var entryForm = document.querySelector('form');
+var form = document.querySelector('form');
+var entryForm = document.getElementById('entry-form');
+var h4 = document.getElementById('no-entries');
 
 function getImage(event) {
   imageEntry.setAttribute('src', imageURL.value);
@@ -10,16 +12,16 @@ imageURL.addEventListener('input', getImage);
 
 function saveEntry(event) {
   event.preventDefault();
-  var entryData = new FormData(entryForm);
+  var entryData = new FormData(form);
   var dataObject = Object.fromEntries(entryData);
   dataObject.entryId = data.nextEntryId;
   data.nextEntryId++;
   data.entries.push(dataObject);
   imageEntry.setAttribute('src', 'images/placeholder-image-square.jpg');
-  entryForm.reset();
+  form.reset();
 }
 
-entryForm.addEventListener('submit', saveEntry);
+form.addEventListener('submit', saveEntry);
 
 function renderEntry(entry) {
   var newDivContainer = document.createElement('div');
@@ -49,14 +51,67 @@ function renderEntry(entry) {
   entryNotes.textContent = entry.notes;
   newRowColHalfText.appendChild(entryNotes);
 
-  var list = document.querySelector('ul');
+  var entryListItem = document.createElement('li');
+  entryListItem.appendChild(newDivContainer);
 
-  for (let i = 0; i < data.entries.length; i++) {
-    var journalEntry = data.entries[i];
-    list.appendChild(journalEntry);
-  }
-
-  return newDivContainer;
+  return entryListItem;
 }
 
-document.addEventListener('DOMContentLoaded', renderEntry);
+var entryListContainer = document.querySelector('ul');
+
+document.addEventListener('DOMContentLoaded', generateDomTree);
+function generateDomTree(event, view) {
+  if (!view) {
+    view = sessionStorage.getItem('view');
+  }
+  viewSwap(view);
+  for (let i = 0; i < data.entries.length; i++) {
+    var entry = data.entries[i];
+    var entryElement = renderEntry(entry);
+    entryListContainer.insertBefore(entryElement, entryListContainer.firstChild);
+  }
+  if (data.entries.length === 0) {
+    toggleNoEntries(true);
+  } else {
+    toggleNoEntries(false);
+  }
+}
+
+function toggleNoEntries(show) {
+  if (show) {
+    h4.classList.remove('hidden');
+  } else {
+    h4.className = 'hidden';
+  }
+}
+
+function viewSwap(view) {
+  var form = document.getElementById('entry-form');
+  var entries = document.getElementById('entriesDiv');
+
+  if (view === 'entries') {
+    form.className = 'hidden';
+    entries.classList.remove('hidden');
+  } else if (view === 'entry-form') {
+    form.classList.remove('hidden');
+    entries.className = 'hidden';
+  }
+
+  sessionStorage.setItem('view', view);
+}
+
+function entriesView() {
+  var view = '';
+  if (entryForm.className === 'hidden') {
+    view = 'entry-form';
+  } else {
+    view = 'entries';
+  }
+  viewSwap(view);
+}
+
+var entriesNav = document.getElementById('navbar-entries');
+var newButton = document.getElementById('new-button');
+
+entriesNav.addEventListener('click', entriesView);
+newButton.addEventListener('click', entriesView);
