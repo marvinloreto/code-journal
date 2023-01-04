afa82eb2eb0df407521a59a30ae5174d26e0960e
Marvin Loreto <marvinjloreto@gmail.com>
2023-01-04 05:49:54 +0000

    Working on ul event listener for editing

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ var imageURL = document.getElementById('photoURL');
 var form = document.querySelector('form');
 var entryForm = document.getElementById('entry-form');
 var h4 = document.getElementById('no-entries');
+var entryListContainer = document.querySelector('ul');
+
+var entryTitle = document.querySelector('.title');
+var entryNotes = document.querySelector('.notes');
+var heading = document.querySelector('h1');
 
 function getImage(event) {
   imageEntry.setAttribute('src', imageURL.value);
@@ -47,17 +52,20 @@ function renderEntry(entry) {
   entryTitle.textContent = entry.title;
   newRowColHalfText.appendChild(entryTitle);
 
+  var pencil = document.createElement('i');
+  pencil.className = 'fa-solid fa-pencil';
+  entryTitle.appendChild(pencil);
+
   var entryNotes = document.createElement('p');
   entryNotes.textContent = entry.notes;
   newRowColHalfText.appendChild(entryNotes);
 
   var entryListItem = document.createElement('li');
   entryListItem.appendChild(newDivContainer);
+  entryListItem.setAttribute('data-entry-id', entry.entryId);
 
   return entryListItem;
 }
-
-var entryListContainer = document.querySelector('ul');
 
 document.addEventListener('DOMContentLoaded', generateDomTree);
 function generateDomTree(event, view) {
@@ -115,3 +123,27 @@ var newButton = document.getElementById('new-button');
 
 entriesNav.addEventListener('click', entriesView);
 newButton.addEventListener('click', entriesView);
+
+var ul = document.querySelector('.entry-list');
+
+function editEntries(event) {
+  var pencilTarget = event.target;
+  if (pencilTarget.tagName === 'I') {
+    var entryItem = pencilTarget.closest('li');
+    var entryId = entryItem.getAttribute('data-entry-id');
+    for (let i = 0; i < data.entries.length; i++) {
+      if (data.entries[i].entryId === parseInt(entryId, 10)) {
+        data.editing = data.entries[i];
+      }
+    }
+    imageURL.value = data.editing.imageURL;
+    entryTitle.value = data.editing.title;
+    entryNotes.value = data.editing.notes;
+    imageEntry.setAttribute('src', data.editing.imageURL);
+    heading.textContent = 'Edit Entry';
+    viewSwap('entry-form');
+  }
+  console.log(entryNotes);
+}
+
+ul.addEventListener('click', editEntries);
